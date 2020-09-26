42fadebed3bfec6d6212e064cf17b085d1712dc1
yyz <110@qq.com>
2020-09-26 15:36:19 +0800

    完成用户列表功能的开发

---
+++ src/plugins/element.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import { Button,Form, FormItem,Input,Message,Container,
+    Header,Aside, Main,Menu,Submenu,MenuItemGroup,MenuItem,
+    Breadcrumb,BreadcrumbItem,Card,Col,Row,Table,TableColumn,Switch,Tooltip,MessageBox ,Pagination,Dialog
+} from 'element-ui'
+
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(TableColumn)
+Vue.use(Table)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
+
+ 
+// Vue.use(Message)
+Vue.prototype.$message=Message
+Vue.prototype.$confirm=MessageBox.confirm
